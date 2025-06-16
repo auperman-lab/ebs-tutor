b726e5bd3e9e1bf05454fe2b28663e225fb9aa9b
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-06-16 21:01:59 +0300

    refactor: auth header refactor

--- apps/admin/src/layout/auth/AuthLayout.tsx
+++ apps/admin/src/layout/auth/AuthLayout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import {  Layout } from 'antd';
-import "./AuthLayout.scss"
 import { AuthHeader } from '../../features/auth/components';
+import { useStyles } from "./AuthLayoutStyles"
+
 
 export const AuthLayout = () => {
   const {  Content } = Layout;
+  const { styles } = useStyles();
+
   return (
-    <Layout className="auth-layout">
+    <Layout className={styles.authLayout}>
       <AuthHeader/>
       <Content>
         <Outlet />
