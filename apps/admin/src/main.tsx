f7714af3057155f297878264968b93b8d2a67081
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-06-17 09:52:04 +0300

    feat: add auth context and respective functions

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Router } from './router/router';
-import { theme } from './styles';
-import { StyleProvider, ThemeProvider } from 'antd-style';
-import { QueryClientContext } from '@context';
-import '@ant-design/v5-patch-for-react-19';
-import './styles.scss';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/router";
+import { theme } from "./styles";
+import { StyleProvider, ThemeProvider } from "antd-style";
+import { QueryClientContext, AuthProvider } from "@context";
+import "@ant-design/v5-patch-for-react-19";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientContext>
+        <AuthProvider>
 
           <StyleProvider>
             <ThemeProvider theme={theme}>
@@ -23,6 +23,7 @@ root.render(
             </ThemeProvider>
           </StyleProvider>
 
+        </AuthProvider>
       </QueryClientContext>
     </BrowserRouter>
   </StrictMode>
