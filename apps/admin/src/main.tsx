92b2c68742febae67d0561d4d0c939794a2b9eec
KARLDFILD <karldfild@mail.ru>
2025-06-17 11:40:41 +0300

    feat: styles for not found page

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./router/router";
-import { theme } from "./styles";
-import { StyleProvider, ThemeProvider } from "antd-style";
-import { QueryClientContext } from "@context";
-import "@ant-design/v5-patch-for-react-19";
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router/router';
+import { theme } from './styles';
+import { StyleProvider, ThemeProvider } from 'antd-style';
+import { QueryClientContext } from '@context';
+import '@ant-design/v5-patch-for-react-19';
+import './styles.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
@@ -22,5 +23,5 @@ root.render(
         </StyleProvider>
       </QueryClientContext>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
