ed77274bc98c69a794c61768b2cf9573c6a0b134
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-06-20 16:53:22 +0300

    refactor: opt in for relative width for dashboard

--- apps/admin/src/features/dashboard/pages/MainPage/MainPage.tsx
+++ apps/admin/src/features/dashboard/pages/MainPage/MainPage.tsx
@@ -1,8 +1,10 @@
-import { Col, Layout, Row } from "antd";
+import { Col, Flex, Layout, Row } from "antd";
 import { ActivityCard, ProfileCard, StatCard } from "../../components";
 //@ts-ignore
 import { ChartBar, ChatCircleDots, CreditCard, Gear } from "@phosphor-icons/react";
 import { RatingCard } from "../../components/RatingCard";
+
+
 
 
 export const MainPage = () => {
@@ -16,37 +18,38 @@ export const MainPage = () => {
   ];
 
   return (
-    <Content style={{ padding: "0 64px", paddingTop: "24px" }}>
-      <Row gutter={[24, 24]}>
-        {stats.map((item, index) => (
-          <StatCard key={`${index}-${item.title}`} color={item.color} title={item.title} quantity={item.quantity}
-                    icon={item.icon} />
-        ))}
-        {/*row1*/}
-        <Col span={24}>
-          <ProfileCard />
-        </Col>
-        {/*row2*/}
-        <Col span={8}>
+    <Content>
+      <Flex  style={{  paddingTop: "24px" }} vertical={true} align="center" justify="center">
+        <Row  style={{ width: "85%"}}  gutter={[24, 24]}>
+          {stats.map((item, index) => (
+            <StatCard key={`${index}-${item.title}`} color={item.color} title={item.title} quantity={item.quantity}
+                      icon={item.icon} />
+          ))}
+          {/*row1*/}
+          <Col span={24}>
+            <ProfileCard />
+          </Col>
+          {/*row2*/}
+          <Col span={8}>
+            <ActivityCard />
+          </Col>
 
-          <ActivityCard />
-        </Col>
+          <Col span={10}>
+            <div style={{ height: "420px", backgroundColor: "#000000", color: "white", width: "100%" }}>revenue</div>
 
-        <Col span={10}>
-          <div style={{ height: "420px", backgroundColor: "#000000", color: "white", width: "100%" }}>revenue</div>
-        </Col>
-        <Col span={6}>
-          <div style={{ height: "420px", backgroundColor: "#000000", color: "white", width: "100%" }}>profile view</div>
-        </Col>
-        {/*row3*/}
-        <Col span={10}>
-          <RatingCard/>
-        </Col>
-        <Col span={14}>
-          <div style={{ height: "480px", backgroundColor: "#000000", color: "white", width: "100%" }}>overview</div>
-        </Col>
-      </Row>
-
+          </Col>
+          <Col span={6}>
+            <div style={{ height: "420px", backgroundColor: "#000000", color: "white", width: "100%" }}>profile view</div>
+          </Col>
+          {/*row3*/}
+          <Col span={10}>
+            <RatingCard />
+          </Col>
+          <Col span={14}>
+            <div style={{ height: "480px", backgroundColor: "#000000", color: "white", width: "100%" }}>overview</div>
+          </Col>
+        </Row>
+      </Flex>
     </Content>
   );
 };
