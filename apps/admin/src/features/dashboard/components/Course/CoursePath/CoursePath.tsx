0fcdef0ffbc605cd6f64e19e0e192ca2f891f339
Sergheev <ivansergheev.stfu@gmail.com>
2025-06-25 10:18:59 +0300

    fix: added breabcrumb component

--- apps/admin/src/features/dashboard/components/Course/CoursePath/CoursePath.tsx
+++ apps/admin/src/features/dashboard/components/Course/CoursePath/CoursePath.tsx
@@ -1,17 +1,30 @@
-import { Flex } from 'antd';
+import { Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
+import { routes } from '@const';
 import { useStyles } from './styles';
-import { Typography } from 'antd';
-
-const { Text } = Typography;
 
 export const CoursePath = () => {
   const { styles } = useStyles();
   return (
-    <Flex vertical={true} className={styles.mainPart}>
-      <Text type="secondary">
-        Course / My Course / Development / Web Development /{' '}
-        <Text>2021 Complete Python Bootcamp From Zero to Hero in Python</Text>
-      </Text>
-    </Flex>
+    <Breadcrumb
+      className={styles.mainPart}
+      items={[
+        {
+          title: <Link to={routes.main}>Dashboard</Link>,
+        },
+        {
+          title: <Link to={routes.courses}>My Courses</Link>,
+        },
+        {
+          title: 'Development',
+        },
+        {
+          title: 'Web Development',
+        },
+        {
+          title: '2021 Complete Python Bootcamp From Zero to Hero in Python',
+        },
+      ]}
+    />
   );
 };
