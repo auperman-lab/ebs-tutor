5afa67ef60e53a82418a7780a65df7801f7f116a
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-06-27 15:15:40 +0300

    refactor: add changePassword request to auth

--- apps/admin/src/api/api.ts
+++ apps/admin/src/api/api.ts
@@ -1,9 +1,7 @@
 import { auth } from "./auth";
-import { changePassword } from "./changePassword";
 import { user } from "./user";
 
 export const api = {
   auth,
-  changePassword,
   user
 };
--- apps/admin/src/api/user.ts
+++ apps/admin/src/api/user.ts
@@ -1,8 +1,9 @@
 import { axiosInstance } from "./axios/instance";
 import { apiEndpoints } from "@const";
+import { UserInfoEndpointResponse } from "@types";
 
 export const user = {
-  get: async (id: string) => {
+  get: async (id: number) : Promise<UserInfoEndpointResponse> => {
     const { data } = await axiosInstance.get(apiEndpoints.getUser + `/${id}`);
     return data.data;
   },
