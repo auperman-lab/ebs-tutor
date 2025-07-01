f53d541692321d4f0fb38c62be78892ab34008a8
Sergheev <ivansergheev.stfu@gmail.com>
2025-07-01 09:07:18 +0300

    refactor: flex box added

--- apps/admin/src/features/create-course/components/AdvanceInformation/AdvanceInformation.tsx
+++ apps/admin/src/features/create-course/components/AdvanceInformation/AdvanceInformation.tsx
@@ -13,25 +13,28 @@ export const AdvanceInformation = () => {
       <Title level={4}>Advance Information</Title>
       <Divider orientationMargin={32} />
       <Form>
-        <Flex className={styles.uploadContainer}>
-          <UploadFiles
-            title="Course Thumbnail"
-            description="Upload your course Thumbnail here. Important guidelines: 1200x800 pixels or 12:8 Ratio. Supported format: .jpg, .jpeg, or .png"
-            acceptedTypes={['image/jpeg', 'image/png']}
-            maxSizeMB={2}
-            buttonText="Upload Image"
-            preview={true}
-            previewImage={<ImagePreview />}
-          />
-          <UploadFiles
-            title="Course Trailer"
-            description="Students who watch a well-made promo video are 5X more likely to enroll in your course. We've seen that statistic go up to 10X for exceptionally awesome videos."
-            acceptedTypes={['video/mp4', 'video/webm', 'video/ogg']}
-            maxSizeMB={50}
-            buttonText="Upload Video"
-            preview={true}
-            previewImage={<VideoPreview />}
-          />
+        <Flex vertical gap={18}>
+          <Flex className={styles.uploadContainer}>
+            <UploadFiles
+              title="Course Thumbnail"
+              description="Upload your course Thumbnail here. Important guidelines: 1200x800 pixels or 12:8 Ratio. Supported format: .jpg, .jpeg, or .png"
+              acceptedTypes={['image/jpeg', 'image/png']}
+              maxSizeMB={2}
+              buttonText="Upload Image"
+              preview={true}
+              previewImage={<ImagePreview />}
+            />
+            <UploadFiles
+              title="Course Trailer"
+              description="Students who watch a well-made promo video are 5X more likely to enroll in your course. We've seen that statistic go up to 10X for exceptionally awesome videos."
+              acceptedTypes={['video/mp4', 'video/webm', 'video/ogg']}
+              maxSizeMB={50}
+              buttonText="Upload Video"
+              preview={true}
+              previewImage={<VideoPreview />}
+            />
+          </Flex>
+          <Divider orientationMargin={32} />
         </Flex>
       </Form>
     </Flex>
