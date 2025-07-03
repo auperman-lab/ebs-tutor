462f601b13f8939eda5da3812cafb7788aac46d8
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-03 17:34:40 +0300

    fix: minor fixes

--- apps/client/src/features/auth/components/RegistrationImageContainer/styles.ts
+++ apps/client/src/features/auth/components/RegistrationImageContainer/styles.ts
@@ -1,6 +1,6 @@
 import { createStyles } from 'antd-style';
 
-export const useStyles = createStyles(({ token, responsive }) => ({
+export const useStyles = createStyles(({ responsive }) => ({
   salyPhoto: {
     width: '100%',
     backgroundColor: '#ebebff',
