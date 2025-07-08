c9119d657359988d0731400f0b7e3da28d74e28d
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-08 13:27:20 +0300

    feat: cosmetic changes to course list page

--- apps/client/src/features/courseList/pages/CourseListPage.tsx
+++ apps/client/src/features/courseList/pages/CourseListPage.tsx
@@ -1,48 +1,49 @@
-import { Col, Row, Spin } from "antd";
+import { Col, Row, Spin } from 'antd';
 import { CourseCard, Filter, ListHeader } from '@clientFeatures/courseList';
-import { api } from "@clientApi";
-import { useQuery } from "@tanstack/react-query";
-import { LoadingOutlined } from "@ant-design/icons";
+import { api } from '@clientApi';
+import { useQuery } from '@tanstack/react-query';
+import { LoadingOutlined } from '@ant-design/icons';
+import { useStyles } from './styles';
 
 
 export const CourseListPage = () => {
 
-  const { data: courses, isLoading, isError } = useQuery({
-    queryKey: ["myCourses", {}],
-    queryFn: () => api.courses.getAllCourses({}),
-  });
+	const {styles} = useStyles();
 
-  if (isLoading) return (<Spin indicator={<LoadingOutlined spin />} size="large" />);
-  if (isError) return <div>error of course</div>;
+	const { data: courses, isLoading, isError } = useQuery({
+		queryKey: ['myCourses', {}],
+		queryFn: () => api.courses.getAllCourses({}),
+	});
 
-  return (
-    <div>
-      <Row gutter={[24, 24]} style={{ width: "100%" }}>
+	if (isLoading) return (<Spin indicator={<LoadingOutlined spin />} size="large" />);
+	if (isError) return <div>error of course</div>;
 
-        <Col span={24}>
-          <ListHeader/>
-        </Col>
+	return (
+		<Row gutter={[24, 24]} className={styles.wrapper}>
 
-        <Col span={6}>
-            <Filter />
-        </Col>
+			<Col span={24}>
+				<ListHeader />
+			</Col>
 
-        <Col span={18}>
-          <Row gutter={[24, 24]} style={{ width: "100%" }}>
-            {courses!.data.map((item) => (
-              <CourseCard
-                key={item.id}
-                image_url={item.image_url}
-                title={item.title}
-                id={item.id}
-                categories={item.categories}
-                users_count={item.users_count}
-                price={item.product?.price}
-              />
-            ))}
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  );
+			<Col span={6}>
+				<Filter />
+			</Col>
+
+			<Col span={18}>
+				<Row gutter={[24, 24]}>
+					{courses!.data.map((item) => (
+						<CourseCard
+							key={item.id}
+							image_url={item.image_url}
+							title={item.title}
+							id={item.id}
+							categories={item.categories}
+							users_count={item.users_count}
+							price={item.product?.price}
+						/>
+					))}
+				</Row>
+			</Col>
+		</Row>
+	);
 };
