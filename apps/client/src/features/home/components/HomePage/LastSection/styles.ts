f1a9542bb5eb692511b22fe965bd9dcdad0e3f94
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-15 18:41:54 +0300

    fix: change last section height

--- apps/client/src/features/home/components/HomePage/LastSection/styles.ts
+++ apps/client/src/features/home/components/HomePage/LastSection/styles.ts
@@ -3,7 +3,7 @@ import { createStyles } from 'antd-style';
 export const useStyles = createStyles(({ token }) => ({
 	wrapper: {
 		width: '100vw',
-		height: '200vh',
+		height: '150vh',
 		background: `linear-gradient(to bottom, ${token.common.gray} 0%, ${token.common.gray} 50%, ${token.common.white} 50%, ${token.common.white} 100%)`,
 
 	},
