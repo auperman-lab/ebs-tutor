1d407e8412639247496262a249a4d6f89cecc474
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-15 14:43:01 +0300

    refactor: fix styles , and console errors

--- apps/client/src/styles/GlobalStyles.ts
+++ apps/client/src/styles/GlobalStyles.ts
@@ -2,18 +2,6 @@ import { createGlobalStyle } from 'antd-style';
 
 export const GlobalStyle = createGlobalStyle(
   ({ theme }) => `
-    .ql-formats {
-      margin: 0px !important
-    }
-
-    .ql-container , .ql-toolbar {
-      border-color: #E9EAF0  !important
-    }
-
-    .ql-toolbar {
-      border-top: none !important
-    }
-
     body, 
     h1,
     h2,
