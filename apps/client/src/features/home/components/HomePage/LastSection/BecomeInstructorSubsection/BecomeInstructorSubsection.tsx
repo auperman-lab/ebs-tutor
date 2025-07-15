ffda1456200d86e259aa92ef04ac187c5e163ccf
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-15 16:43:54 +0300

    feat: become instructor subsection for homepage done

--- apps/client/src/features/home/components/HomePage/LastSection/BecomeInstructorSubsection/BecomeInstructorSubsection.tsx
+++ apps/client/src/features/home/components/HomePage/LastSection/BecomeInstructorSubsection/BecomeInstructorSubsection.tsx
@@ -1,40 +1,100 @@
-import { Button, Flex } from 'antd';
+import { Button, Flex, List } from 'antd';
 import { ArrowRight, BecomeInstructorImg } from '@client/assets';
 import { useStyles } from './styles';
 import { useNavigate } from 'react-router-dom';
 import { routes } from '@client/const';
+import { useTheme } from 'antd-style';
 
 export const BecomeInstructorSubsection = () => {
 	const { styles } = useStyles();
 	const navigate = useNavigate();
+	const palette = useTheme();
+
+
+	const learnData = [
+		{
+			title: 'Apply to become instructor',
+			backgroundColor: palette.secondary.secondary100,
+			color: palette.secondary.secondary500
+		},
+		{
+			title: 'Build & edit your profile',
+			backgroundColor: palette.danger.danger100,
+			color: palette.danger.danger500
+		},
+		{
+			title: 'Create your new course',
+			backgroundColor: palette.warning.warning100,
+			color: palette.warning.warning500
+		},
+		{
+			title: 'Start teaching & earning',
+			backgroundColor: palette.success.success100,
+			color: palette.success.success500
+		},
+
+	];
+
 
 	const onClick = () => {
 		navigate(routes.becomeInstructor);
 	};
 
 	return (
-		<Flex align="center" className={styles.wrapperLeft}>
-			<Flex gap={24} vertical align="start" justify="center">
-				<Flex gap={12} vertical align="start" justify="center" className={styles.infoWrapper}>
-					<div className={styles.title}>Become an Instructor</div>
-					<div className={styles.text}>Instructors from around the world teach millions of students on Udemy. We provide
-						the tools and skills to
-						teach what you love.
-					</div>
-				</Flex>
-				<Button size="large" className={styles.button} onClick={onClick}>
-					<Flex gap={8} align="center">
-						Start Teaching
-						<ArrowRight />
+		<Flex gap={20} justify="space-between" align="center" className={styles.subsectionWrapper}>
+			<Flex align="center" className={styles.wrapperLeft}>
+				<Flex gap={24} vertical align="start" justify="center">
+					<Flex gap={12} vertical align="start" justify="center" className={styles.infoWrapper}>
+						<div className={styles.title}>Become an Instructor</div>
+						<div className={styles.text}>Instructors from around the world teach millions of students on Udemy. We
+							provide
+							the tools and skills to
+							teach what you love.
+						</div>
 					</Flex>
-				</Button>
+					<Button size="large" className={styles.button} onClick={onClick}>
+						<Flex gap={8} align="center">
+							Start Teaching
+							<ArrowRight />
+						</Flex>
+					</Button>
 
+				</Flex>
+
+				<img
+					className={styles.image}
+					src={BecomeInstructorImg}
+				/>
 			</Flex>
 
-			<img
-				className={styles.image}
-				src={BecomeInstructorImg}
-			/>
+			<Flex gap={27} vertical className={styles.wrapperRight} align='start' >
+				<div className={styles.title}>Your teaching & earning steps</div>
+				<List
+					grid={{
+						column: 2,
+						gutter: 20
+					}}
+					dataSource={learnData}
+					renderItem={(item, index) => (
+						<List.Item key={item.title}>
+							<Flex gap={16} align='center'>
+								<Flex
+									align='center'
+									justify='center'
+									className={styles.indexCircle}
+									style={{
+										backgroundColor: item.backgroundColor,
+										color: item.color
+									}}
+								>
+									{index}
+								</Flex>
+								<div className={styles.text}>{item.title}</div>
+							</Flex>
+						</List.Item>
+					)}
+				/>
+			</Flex>
 		</Flex>
 	);
 };
--- apps/client/src/features/home/components/HomePage/LastSection/BecomeInstructorSubsection/styles.ts
+++ apps/client/src/features/home/components/HomePage/LastSection/BecomeInstructorSubsection/styles.ts
@@ -1,14 +1,28 @@
 import { createStyles } from 'antd-style';
 
 export const useStyles = createStyles(({ token, responsive }) => ({
+	subsectionWrapper:{
+		width: '70%',
+
+		[responsive.xl]:{
+			flexDirection: 'column',
+			gap: '40px'
+		}
+	},
 	wrapperLeft: {
-		backgroundColor: token.colorWhite,
 		height: '300px',
 		width: '600px',
 		background: 'linear-gradient(90deg, #CC522B 0%, #FF6636 100%)',
 		color: token.colorWhite,
 		padding: '40px',
 		overflow: 'hidden'
+	},
+	wrapperRight:{
+		backgroundColor: token.colorWhite,
+		height: '300px',
+		width: '600px',
+		color: token.common.black,
+		padding: '40px',
 	},
 	title: {
 		fontWeight: token.fontWeightStrong,
@@ -32,7 +46,18 @@ export const useStyles = createStyles(({ token, responsive }) => ({
 	button: {
 		color: token.colorPrimary,
 		fontWeight: token.fontWeightStrong
+	},
+	indexCircle: {
+		backgroundColor: token.colorPrimary,
+		color: token.colorWhite,
+		borderRadius: '50%',
+		width: '50px',
+		aspectRatio: '1',
+		textAlign: 'center',
+		fontSize: token.fontSizeHeading3,
+
 	}
+
 
 
 
