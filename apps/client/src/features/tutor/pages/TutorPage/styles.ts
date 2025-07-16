4de4d30390fd37019bcbb4d116cbea93889d9a17
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-16 14:49:01 +0300

    feat: add courses to tutor page

---
+++ apps/client/src/features/tutor/pages/TutorPage/styles.ts
@@ -0,0 +1,12 @@
+import { createStyles } from 'antd-style';
+
+export const useStyles = createStyles(({token}) => ({
+	wrapper: {
+		paddingTop: 40,
+	},
+	courseTitle:{
+		fontSize: token.fontSizeHeading4,
+		fontWeight: token.fontWeightStrong
+	},
+
+}));
