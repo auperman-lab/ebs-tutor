4fde6623377876ac144fae0f28f6b9efb9d23d3a
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-07-16 14:15:48 +0300

    feat: create sider for tutorpage

--- apps/client/src/features/tutor/components/TutorPageSider/TutorPageSider.tsx
+++ apps/client/src/features/tutor/components/TutorPageSider/TutorPageSider.tsx
@@ -1,5 +1,26 @@
+import { Flex } from 'antd';
+import { useStyles } from './styles';
+import { useQuery } from '@tanstack/react-query';
+import { api } from '@client/api/api';
+import { useParams } from 'react-router-dom';
+
 export const TutorPageSider = () =>{
+	const {styles} = useStyles();
+	const { id } = useParams();
+
+	if (!id) return <div>No id provided</div>;
+
+	const { data: tutor, isLoading } = useQuery({
+		queryKey: ['tutors'],
+		queryFn: () => api.courses.getTutor(id),
+	});
+
+	if (isLoading) return <div>Loading</div>
+
 	return (
-		<div>sider</div>
+		<Flex gap={24} vertical className={styles.wrapper} align='start'>
+			<div className={styles.title}>ABOUT ME</div>
+			<div className={styles.text}>{tutor?.bio}</div>
+		</Flex>
 	)
 }
