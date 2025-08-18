b19a10c8764f43df477246c949b091151ce55157
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-08-18 19:06:11 +0300

    add cloudinary file db

---
+++ apps/backend/src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, UseInterceptors } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import type { UploadApiOptions, UploadApiResponse } from 'cloudinary';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Readable } from 'stream';
+
+@Injectable()
+export class CloudinaryService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: 'dy4k743ig',
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+    });
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(
+    file: Express.Multer.File,
+    options?: UploadApiOptions
+  ): Promise<UploadApiResponse> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        options,
+        (error, result) => {
+          if (error || result == null) return reject(error);
+          resolve(result);
+        }
+      );
+
+      const stream = Readable.from(file.buffer);
+      stream.pipe(uploadStream);
+    });
+  }
+}
