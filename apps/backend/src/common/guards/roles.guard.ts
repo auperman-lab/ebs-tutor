d5a2526d2445129b1b8a4837c3e677c09ad359c7
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-08-21 16:34:52 +0300

    fix role guard to accept public endpoints

--- apps/backend/src/common/guards/roles.guard.ts
+++ apps/backend/src/common/guards/roles.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../types';
+import { PUBLIC_KEY, ROLES_KEY } from '../types';
 import { Role } from '@prisma/client';
 
 @Injectable()
@@ -8,6 +8,12 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
